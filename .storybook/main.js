95f68598a64d610d5b7c95bcfd6e3038325cf40d
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-04-16 13:14:58 -0300

    feat: add storybook addons and configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,20 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-controls",
+    "@storybook/addon-storysource",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        sourceLoaderOptions: {
+          injectStoryParameters: false,
+        },
+      },
+    },
   ],
-  "framework": "@storybook/react"
-}
+  webpackFinal: async (config) => {
+    return config;
+  },
+};
