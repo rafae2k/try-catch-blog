772408580665996dabcd493965eb63acd44351f5
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-04-16 15:13:02 -0300

    chore(stotybook): config ts paths

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -15,6 +17,12 @@ module.exports = {
     },
   ],
   webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ];
     return config;
   },
 };
