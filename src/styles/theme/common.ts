e7a298aba1d86d5c80eadba0da9f085f80c735fd
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-04-17 15:55:38 -0300

    styles(theme): add common properties

---
+++ src/styles/theme/common.ts
@@ -0,0 +1,18 @@
+import { BaseTheme } from "styled-components";
+
+const common: BaseTheme = {
+  colors: {
+    neutral: {
+      white: "#fff",
+      black: "#000",
+      100: "#1A1D23",
+      90: "#1D232A",
+      80: "#2C343A",
+      50: "#95999C",
+      20: "#D4D4D4",
+      10: "#fdf2f8",
+    },
+  },
+};
+
+export { common };
