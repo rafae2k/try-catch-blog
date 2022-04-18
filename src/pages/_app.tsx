187773cbb6a9f6869d9b495d5aa2ad347f285d40
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-04-18 10:01:22 -0300

    refactor(app): move to app header, globalstyles and theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Header } from "../components/Header";
+import GlobalStyle from "@styles/GlobalStyles";
+import { StyledThemeProvider } from "@styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <StyledThemeProvider>
+      <GlobalStyle />
+      <Header />
+      <Component {...pageProps} />
+    </StyledThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
