8a3e4becffb96691ae1d152fd3a78a94f82bc621
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-04-18 09:59:56 -0300

    feat: add theme definitions

---
+++ src/definitions/Interfaces/styled-components/theme.d.ts
@@ -0,0 +1,50 @@
+import "styled-components";
+
+type PrimaryColors = {
+  100: string;
+  90: string;
+  80: string;
+  50: string;
+  20: string;
+  10: string;
+};
+
+type SecondaryColors = {
+  100: string;
+  90: string;
+  80: string;
+  50: string;
+  20: string;
+  10: string;
+};
+
+type NeutralColors = {
+  white: string;
+  black: string;
+  100: string;
+  90: string;
+  80: string;
+  50: string;
+  20: string;
+  10: string;
+};
+
+declare module "styled-components" {
+  export interface BaseTheme {
+    colors: {
+      neutral: NeutralColors;
+    };
+  }
+
+  export interface DefaultTheme extends BaseTheme {
+    title: string;
+    colors: {
+      neutral: NeutralColors;
+      background: string;
+      heading: string;
+      body: string;
+      primary: PrimaryColors;
+      secondary: SecondaryColors;
+    };
+  }
+}
