b56027a249f5a32261ab9fbf227531ea36f2455b
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-04-18 10:02:46 -0300

    feat(posts): add posts page

---
+++ src/pages/posts.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Posts: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title> Posts | Try...Catch </title>
+      </Head>
+      Posts
+    </div>
+  );
+};
+
+export default Posts;
