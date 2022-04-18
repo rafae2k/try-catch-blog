b3b41c3c3eea9068570508e0ee85d67afbb5f7cb
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-04-18 09:57:10 -0300

    refactor(toggle): implement next link

--- src/components/Header/NavLink/index.tsx
+++ src/components/Header/NavLink/index.tsx
@@ -1,14 +1,24 @@
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/router";
 import { ReactNode } from "react";
 
 import { Container } from "./styles";
 
-export interface NavLinkProps {
+interface NavLinkProps extends LinkProps {
   children: ReactNode;
   isActive?: boolean;
 }
 
 function NavLink({ children, ...args }: NavLinkProps) {
-  return <Container {...args}>{children}</Container>;
+  const { asPath } = useRouter();
+
+  const isActive = asPath === args.href;
+
+  return (
+    <Container isActive={isActive}>
+      <Link {...args}>{children}</Link>
+    </Container>
+  );
 }
 
 export default NavLink;
