d679bd83ea1ba75a7a67fc8547e4f191ff0a619e
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-04-18 09:59:00 -0300

    refactor(header): navlink and navmenu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,26 +1,32 @@
-import SignInButton from "@components/SignInButton";
 import React, { useState } from "react";
-import NavLink from "./NavLink";
+import SignInButton from "@components/SignInButton";
+import Toggle from "@components/Header/Toggle";
+import NavLink from "@components/Header/NavLink";
 import { Container, Logo, HamburgerMenu, Nav, NavMenu } from "./styles";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <Container>
       <Nav>
-        <Logo src="/images/logo.svg" alt="Try...catch logo" />
+        <Link href="/">
+          <Logo src="/images/logo.svg" alt="Try...catch logo" />
+        </Link>
 
         <HamburgerMenu />
 
         <NavMenu>
-          <NavLink isActive>
-            <a href="/">Home</a>
+          <NavLink href="/">
+            <a>Home</a>
           </NavLink>
-          <NavLink>
-            <a href="#">Posts</a>
+          <NavLink href="/posts" prefetch>
+            <a>Posts</a>
           </NavLink>
+          <div>
+            <SignInButton />
+            <Toggle />
+          </div>
         </NavMenu>
-
-        <SignInButton />
       </Nav>
     </Container>
   );
