924bb5e6ca0e710c9e88f36aed55ab051d2b491a
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-04-19 20:56:04 -0300

    feat(wrapper): add page content wrapper

---
+++ src/components/Wrapper/index.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+import { Container } from "./styles";
+
+interface WrapperProps {
+  children: ReactNode;
+}
+
+function Wrapper({ children }: WrapperProps) {
+  return <Container>{children}</Container>;
+}
+
+export default Wrapper;
