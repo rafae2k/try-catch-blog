118c7b2507dc2dea7525fd7df644850edfb73441
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-04-19 20:59:13 -0300

    feat(app): add loading progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,15 @@ import type { AppProps } from "next/app";
 import { Header } from "../components/Header";
 import GlobalStyle from "@styles/GlobalStyles";
 import { StyledThemeProvider } from "@styles/theme";
+import NextNProgress from "nextjs-progressbar";
+import { useTheme } from "@styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { theme } = useTheme();
+
   return (
     <StyledThemeProvider>
+      <NextNProgress color={theme.colors.primary[50]} />
       <GlobalStyle />
       <Header />
       <Component {...pageProps} />
