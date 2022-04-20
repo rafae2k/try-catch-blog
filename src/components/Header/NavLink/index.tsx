525e3084b64e97afc17d5b91492e5efe3289932a
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-04-20 19:12:57 -0300

    refactor: export interface

--- src/components/Header/NavLink/index.tsx
+++ src/components/Header/NavLink/index.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 
 import { Container } from "./styles";
 
-interface NavLinkProps extends LinkProps {
+export interface NavLinkProps extends LinkProps {
   children: ReactNode;
   isActive?: boolean;
 }
