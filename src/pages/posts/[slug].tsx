39dbb0d69f537b9ea0a98133a55543b2aa23fe9b
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-04-20 10:48:10 -0300

    feat(post): fetch post data SSG

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,19 +1,93 @@
 import React from "react";
+import Head from "next/head";
+import { GetStaticPaths, NextPage } from "next";
+import { PrismicRichText } from "@prismicio/react";
+import * as prismicT from "@prismicio/types";
 import { createClient } from "@services/prismic";
+import Wrapper from "@components/Wrapper";
+import { BsCalendarDateFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
 
-const Post: React.FC = () => {
+import { Article, PostInfo } from "./slug.styles";
+
+type Props = {
+  post: {
+    uid: string;
+    title: string;
+    thumbnail: {
+      url: string;
+      alt: string;
+    };
+    content: prismicT.RichTextField;
+    datePublished: string;
+    author: string;
+  };
+};
+
+const Post: NextPage<Props> = ({ post }) => {
   return (
-
+    <>
+      <Head>
+        <title>{post.title} | Try...Catch</title>
+      </Head>
+      <Wrapper>
+        <img src={post.thumbnail.url} alt={post.thumbnail.alt} />
+        <Article>
+          <h1>{post.title}</h1>
+          <PostInfo>
+            <div>
+              <BsCalendarDateFill />
+              <time itemProp="dateCreated datePublished">
+                {new Date(post.datePublished).toLocaleDateString("pt-BR", {
+                  day: "2-digit",
+                  month: "long",
+                  year: "numeric",
+                })}
+              </time>
+            </div>
+            <div>
+              <FaUser />
+              <p itemProp="author publisher name">{post.author}</p>
+            </div>
+          </PostInfo>
+          <PrismicRichText field={post.content} />
+        </Article>
+      </Wrapper>
+    </>
   );
 };
 
-export async function getStaticProps() {
+export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
+  return {
+    paths: [], //indicates that no page needs be created at build time
+    fallback: "blocking", //indicates the type of fallback
+  };
+};
+
+type Params = {
+  params: {
+    slug: string;
+  };
+};
+
+export const getStaticProps = async ({ params }: Params) => {
+  const { slug } = params;
   const client = createClient();
 
+  const post = await client.getByUID("blog-post", slug);
 
-
-  const 
-
-}
+  return {
+    props: {
+      post: {
+        uid: post.uid,
+        title: post.data.slices[0].primary.title[0].text,
+        thumbnail: post.data.thumbnail,
+        content: post.data.slices[0].primary.content,
+        datePublished: post.first_publication_date,
+        author: post.data.author,
+      },
+    },
+  };
+};
 
 export default Post;
