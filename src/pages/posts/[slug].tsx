53545d918f58f2a8e5c6c4cc5cf3d05ed95bee3f
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-04-19 21:01:21 -0300

    feat(posts): add dinamic post page

---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { createClient } from "@services/prismic";
+
+const Post: React.FC = () => {
+  return (
+
+  );
+};
+
+export async function getStaticProps() {
+  const client = createClient();
+
+
+
+  const 
+
+}
+
+export default Post;
