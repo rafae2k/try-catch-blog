085388c462112cd5ca4cbd84c946e8fa69bb98b9
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-04-20 19:14:28 -0300

    styles: add styles

---
+++ src/components/NextPreviousPosts/styles.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding-bottom: 4rem;
+  justify-content: space-between;
+  gap: 2rem;
+
+  div {
+    width: clamp(20rem, 20rem, 70rem);
+    font-size: 2rem;
+    transition: all 1s ease-in;
+    h3 {
+      color: ${({ theme }) => theme.colors.primary[50]};
+      padding-bottom: 2rem;
+    }
+
+    p {
+      font-size: 1.8rem;
+    }
+
+    :hover {
+      div > p {
+        font-weight: bold;
+        color: ${({ theme }) => theme.colors.primary[50]};
+      }
+    }
+  }
+`;
