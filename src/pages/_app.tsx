03abe677f91b97b42e06a896edc3b1d72cee1dd6
Rafael Feitoza <rafaeljr@outlook.com>
2022-04-24 16:59:48 -0300

    feat(_app): add next-auth provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import type { AppProps } from "next/app";
 import GlobalStyle from "@styles/GlobalStyles";
 import { StyledThemeProvider, useTheme } from "@styles/theme";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider as NextAuthSessionProvider } from "next-auth/react";
 import NextNProgress from "nextjs-progressbar";
 import { Header } from "../components/Header";
 
@@ -10,14 +10,14 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const { theme } = useTheme();
 
   return (
-    <SessionProvider session={session}>
+    <NextAuthSessionProvider session={session}>
       <StyledThemeProvider>
         <NextNProgress color={theme.colors.primary[50]} />
         <GlobalStyle />
         <Header />
         <Component {...pageProps} />
       </StyledThemeProvider>
-    </SessionProvider>
+    </NextAuthSessionProvider>
   );
 }
 
