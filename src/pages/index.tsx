d4e1b307b26516828c86d563e3879abc3345f0ce
Rafael Feitoza <rafaeljr@outlook.com>
2022-04-24 19:15:23 -0300

    refactor: tranform posts to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,68 @@
 import React from "react";
+import type { NextPage } from "next";
 import Head from "next/head";
-import { Container } from "@styles/index.styles";
+import Link from "next/link";
+import { PrismicRichText } from "@prismicio/react";
+import { createClient } from "@services/prismic";
+import { Post } from "@definitions/Interfaces/prismic/slices";
+import Wrapper from "@components/Wrapper";
 
-function Home() {
-  return (
-    <Container>
-      <Head>
-        <title> Home | Try...Catch </title>
-      </Head>
-    </Container>
-  );
+import { BsCalendarDateFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+
+import { Article, PostInfo } from "../styles/pages/posts/index.styles";
+
+export type PostsProps = {
+  posts: Post[];
+};
+
+const Posts: NextPage<PostsProps> = ({ posts }) => (
+  <>
+    <Head>
+      <title> Posts | Try...Catch </title>
+    </Head>
+    <Wrapper>
+      {posts?.map((post) => (
+        <Link href={`/${post.uid}`} key={post.uid} passHref>
+          <Article>
+            <PrismicRichText field={post.data.slices[0].primary.title} />
+            <PrismicRichText field={post.data.slices[0].primary.description} />
+            <PostInfo>
+              <div>
+                <BsCalendarDateFill />
+                <time itemProp="dateCreated datePublished">
+                  {new Date(post.first_publication_date).toLocaleDateString(
+                    "pt-BR",
+                    {
+                      day: "2-digit",
+                      month: "long",
+                      year: "numeric",
+                    }
+                  )}
+                </time>
+              </div>
+              <div>
+                <FaUser />
+                <p itemProp="author publisher name">{post.data.author}</p>
+              </div>
+            </PostInfo>
+          </Article>
+        </Link>
+      ))}
+    </Wrapper>
+  </>
+);
+
+export async function getStaticProps() {
+  const client = createClient();
+
+  const { results: posts } = await client.get();
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
 
-export default Home;
+export default Posts;
