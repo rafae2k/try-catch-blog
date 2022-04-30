79dec0008beb24437524c467434258a43e1726ce
Rafael Feitoza <rafaeljr@outlook.com>
2022-04-30 19:18:04 -0300

    feat: add i18n multi-language support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const prismic = require("@prismicio/client");
 
-module.exports = nextConfig;
+const sm = require("./sm.json");
+
+/**
+ * @returns {import('next').NextConfig}
+ */
+module.exports = async () => {
+  const client = prismic.createClient(sm.apiEndpoint);
+
+  const repository = await client.getRepository();
+  const locales = repository.languages.map((lang) => lang.id);
+
+  return {
+    i18n: {
+      locales,
+      defaultLocale: locales[0],
+    },
+    images: {
+      domains: ["images.prismic.io"],
+    },
+  };
+};
