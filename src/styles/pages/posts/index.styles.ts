6534ae5336391138cf88534ca12869ca0bc5cf38
Rafael Feitoza <rafaeljr@outlook.com>
2022-04-30 19:14:55 -0300

    refactor: move postInfo to its own component styles

--- src/styles/pages/posts/index.styles.ts
+++ src/styles/pages/posts/index.styles.ts
@@ -38,27 +38,3 @@ export const Article = styled.article`
     color: ${({ theme }) => theme.colors.body};
   }
 `;
-
-export const PostInfo = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-
-  div {
-    display: flex;
-    align-items: center;
-    gap: 0.5rem;
-    color: ${({ theme }) => theme.colors.neutral[50]};
-
-    p,
-    time {
-      color: ${({ theme }) => theme.colors.neutral[50]};
-      font-size: 1.4rem;
-    }
-
-    svg {
-      height: 2rem;
-      width: auto;
-    }
-  }
-`;
