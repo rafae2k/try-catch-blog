9b92942819404e6329ff9044b5a30b060afb6bb2
Rafael Feitoza <rafaeljr@outlook.com>
2022-05-01 19:34:58 -0300

    feat: add codeSnippet and Image Slices

---
+++ .slicemachine/assets/slices/CodeSnippet/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/CodeSnippet';
+
+export default {
+  title: 'slices/CodeSnippet'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"code_snippet","items":[],"primary":{"codeSnippet":[{"type":"paragraph","text":"Proident et deserunt aute dolor ullamco aute in.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
---
+++ .slicemachine/assets/slices/Image/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Image';
+
+export default {
+  title: 'slices/Image'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"image","items":[],"primary":{"image":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1586952518485-11b180e92764?w=900&h=500&fit=crop"}},"id":"_Default"}} />
+_Default.storyName = 'Default'
